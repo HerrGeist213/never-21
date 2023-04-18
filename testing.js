85bfa5fc601870facebe546a5427c78643024c91
HerrGeist213 <118394610+HerrGeist213@users.noreply.github.com>
2023-04-18 14:39:33 -0400

    Add files via upload

---
+++ testing.js
@@ -0,0 +1,28 @@
+// console.log(4 === 3)  // false
+// console.log(5 > 2)    // true
+// console.log(12 > 12)  // false
+// console.log(3 < 0)    // false
+// console.log(3 >= 3)   // true
+// console.log(11 <= 11) // true
+// console.log(3 <= 2)   // false
+
+
+// console.log(isAlive)
+
+// let age = 21
+
+// if (age <= 20) {
+//     console.log("Get lost, kiddo!")
+// } else {
+//     console.log("You want a boilermaker, old man?")
+// }
+
+// let age = 101
+
+// if (age < 100) {
+//     console.log("Not eligible")
+// } else if (age === 100) {
+//     console.log ("Here is your birthday card from the King!")
+// } else {
+//     console.log("Not eligible, you have already gotten one.")
+// }
